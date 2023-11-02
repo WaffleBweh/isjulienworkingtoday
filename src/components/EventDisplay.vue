ed5780bfd3f3cbe4c85fda2acc5ea3bcc6bec555
CapitaineToinon <antoinesutter@gmail.com>
2023-11-02 21:24:16 +0100

    Various improvements
    - Added tomorrow button
    - Fixed light theme
    - Formatted with prettier

--- src/components/EventDisplay.vue
+++ src/components/EventDisplay.vue
@@ -1,59 +1,56 @@
 <script setup>
-import { ref } from 'vue'
-
 defineProps({
-  event: Object
+	event: Object,
 })
-
 </script>
 
 <template>
-  <div class="card">
-    <h2>From {{ new Intl.DateTimeFormat('default', {
-      hour: 'numeric',
-      minute: 'numeric',
-    }).format(new Date(event.start))
-    }} to {{ new Intl.DateTimeFormat('default', {
-  hour: 'numeric',
-  minute: 'numeric',
-}).format(new Date(event.end))
-}}</h2>
-    <h3>Julien is working @ {{ event.title }}</h3>
-    <a target="_blank" :href="`https://www.google.com/maps/search/${encodeURIComponent(event.location)}/`">{{
-      event.location }}</a>
-  </div>
+	<div class="card">
+		<h2>
+			From
+			{{
+				new Intl.DateTimeFormat("default", {
+					hour: "numeric",
+					minute: "numeric",
+				}).format(new Date(event.start))
+			}}
+			to
+			{{
+				new Intl.DateTimeFormat("default", {
+					hour: "numeric",
+					minute: "numeric",
+				}).format(new Date(event.end))
+			}}
+		</h2>
+		<h3>Julien is working @ {{ event.title }}</h3>
+		<a
+			target="_blank"
+			:href="`https://www.google.com/maps/search/${encodeURIComponent(
+				event.location,
+			)}/`"
+			>{{ event.location }}</a
+		>
+	</div>
 </template>
 
-
 <style scoped>
-  .card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 4px;
-  }
+.card {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 1rem;
+	margin: 1rem;
+	border-radius: 4px;
+}
 
-  h2, h3 {
-    font-size: 1.25rem;
-    margin: 0.5rem 0;
-  }
+h2,
+h3 {
+	font-size: 1.25rem;
+	margin: 0.5rem 0;
+}
 
-  a {
-    color: #0077cc;
-    text-decoration: none;
-  }
-
-  @media (max-width: 600px) {
-    h2, h3 {
-      font-size: 0.75rem;
-      margin: 0.25rem;
-    }
-
-    a {
-      font-size: 0.65rem;
-      margin: 1rem;
-    }
-  }
+a {
+	color: #0077cc;
+	text-decoration: none;
+}
 </style>
